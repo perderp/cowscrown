bec07c2b005c80bba642f4126cb4adc178c47e39
perderp <chironjasphercainap@gmail.com>
2021-03-23 21:35:24 +0800

    Changes: Added Animations

--- src/js/pc/js/app.js
+++ src/js/pc/js/app.js
@@ -9,3 +9,47 @@ $(document).ready(function(){
       }
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    // OFFSET TOP OF EACH SECTIONS 
+    const hero = document.querySelector('.hero').offsetTop;
+    const concept = document.querySelector('.concept').offsetTop;
+    const menu = document.querySelector('.menu').offsetTop;
+    const access = document.querySelector('.access').offsetTop;
+    // ADDING CLASS TO THIS QUERY
+    const con_burger = document.querySelector('.con_burger');
+    const tk_burger = document.querySelector('.tk_burger');
+    //change color background on SP
+    const nav_m_wrap = document.querySelector('.nav_m_wrap');
+    if(window.pageYOffset === hero){
+        con_burger.classList.add('fade-left-in');
+        con_burger.classList.remove('fade-left-out');
+    }
+    window.onscroll= () =>{
+        const pageY = window.pageYOffset; 
+        console.log(pageY);
+        // if(pageY > 0){
+        //     if(window.innerWidth <= 768 && window.innerWidth >= 320){
+        //         nav_m_wrap.style.position ="fixed";
+        //         nav_m_wrap.style.background ="rgba(0,0,0,.3)";
+        //     }else{
+        //         nav_m_wrap.style.position ="absolute";
+        //         nav_m_wrap.style.background = "transparent";
+        //     }
+        // }
+        if(pageY >= hero && pageY <= (concept-1) ){
+            con_burger.classList.add('fade-left-in');
+            con_burger.classList.remove('fade-left-out');
+        }else{
+            con_burger.classList.add('fade-left-out');
+            con_burger.classList.remove('fade-left-in');
+        }
+        if(pageY >= menu && pageY <= (access) ){
+            tk_burger.classList.add('fade-right-in');
+            tk_burger.classList.remove('fade-right-out');
+        }else{
+            tk_burger.classList.add('fade-right-out');
+            tk_burger.classList.remove('fade-right-in');
+        }
+    }
+});
